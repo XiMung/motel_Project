f23945f77174e8692d3cd2c7b926489cb78ba233
Quang Hieu <hieunguyen@appvity.com>
2021-08-02 14:50:53 +0700

    commit

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,9 +1,14 @@
 let multer = require('multer');
 let path = require('path');
+const fs = require('fs');
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        let outputPath = path.resolve(__dirname, '../public/upload/')
+        var outputPath = path.resolve(__dirname + '../../public/');
+        if (!fs.existsSync(outputPath)) {
+            fs.mkdirSync(outputPath);
+        }
+        // let outputPath = path.resolve(__dirname, '../public/upload/')
         cb(null, outputPath)
     },
     filename: function (req, file, cb) {
