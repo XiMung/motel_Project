d244faf71d05f1ef8b0e60a6a31bc2b1e14e4aed
Quang Hieu <hieunguyen@appvity.com>
2021-09-12 16:54:00 +0700

    commit

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,45 +3,78 @@ var queryParser = require('../configs/queryParser');
 const tv4 = require('tv4');
 const HomeSchema = require('../schemas/Home.schema.json');
 
+const AWS = require('aws-sdk');
+const BUCKET_NAME = 'testimagemotel';
+const IAM_USER_KEY = 'AKIA4UAX4PYRWEMHKD7W';
+const IAM_USER_SECRET = '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';
+
 module.exports = {
     create: create,
     get: get
 }
 var query = {};
 
+const s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM_USER_SECRET
+});
+
+
 async function create(req, res) {
     try {
-        var body = req.body;
+        // var body = req.body;
         // var schema = tv4.validateResult(body, HomeSchema, true, true);
         // if (!schema.valid) {
         //     return res.status(400).json({ error: `${schema.error.dataPath}: ${schema.error.message}` });
         // }
-        if (!body.title) return res.json({ error: true, message: "Title empty" });
-        if (!body.description) return res.json({ error: true, message: "Description empty" });
-        if (!body.address) return res.json({ error: true, message: "Address empty" });
-        if (!body.from) return res.json({ error: true, message: "Appointment empty" });
-        if (!body.to) return res.json({ error: true, message: "Appointment empty" });
+        // if (!body.title) return res.json({ error: true, message: "Title empty" });
+        // if (!body.description) return res.json({ error: true, message: "Description empty" });
+        // if (!body.address) return res.json({ error: true, message: "Address empty" });
+        // if (!body.from) return res.json({ error: true, message: "Appointment empty" });
+        // if (!body.to) return res.json({ error: true, message: "Appointment empty" });
 
-        var image = req.files;
-        if (!image || image.length == 0) return res.json({ error: true, message: "Image empty" });
+        // var image = req.files;
+        // if (!image || image.length == 0) return res.json({ error: true, message: "Image empty" });
+        // var A = [];
+        // for (let i = 0; i < image.length; i++) {
+        //     A[i] = image[i].filename;
+        // }
+        var images = req.files;
         var A = [];
-        for (let i = 0; i < image.length; i++) {
-            A[i] = image[i].filename;
-        }
+        await asyncForEach(images, async image => {
+            const params = {
+                Bucket: BUCKET_NAME,
+                Key: `${Date.now()}_${image.originalname}`,
+                Body: image.buffer
+            };
+            try {
+                s3bucket.upload(params, function (err, data) {
+                    if (err) {
+                        console.log('error in callback');
+                        return err;
+                    }
+                    console.log('success');
+                    A.push(data.Location);
+                });
+            } catch (error) {
+                console.log("hieu", error);
+                return error.message;
+            }
+        })
 
         if (!body.userId) return res.json({ error: true, message: "userId empty" });
         var query = {
-            title: body.title,
-            description: body.description,
-            address: body.address,
-            appointment: {from: body.from, to: body.to},
+            // title: body.title,
+            // description: body.description,
+            // address: body.address,
+            // appointment: {from: body.from, to: body.to},
             image: A,
-            verify: 0,
-            point: 0,
-            reason: "",
-            lat: body.lat,
-            long: body.long,
-            userId: body.userId
+            // verify: 0,
+            // point: 0,
+            // reason: "",
+            // lat: body.lat,
+            // long: body.long,
+            // userId: body.userId
         }
 
         var home = new homeModel(query);
--- src/routers/home.router.js
+++ src/routers/home.router.js
@@ -4,9 +4,11 @@ var HomeController = require('../controllers/home.controller');
 var jwt = require('jsonwebtoken');
 const passport = require('passport');
 var passportConfig = require('../middleware/passport'); // bắt buộc
-var { upload } = require("../middleware/multer.middleware");
 
-homeRouter.post("/create", passport.authenticate('jwt', { session: false }), upload.array('image', 10), (req, res) => HomeController.create(req, res));
+var multer = require('multer');
+const upload = multer();
+
+homeRouter.post("/create",  upload.any(), (req, res) => {HomeController.create(req, res)});
 homeRouter.get("/get",  (req, res) => HomeController.get(req, res));
 //passport.authenticate('jwt', { session: false }),
 module.exports = homeRouter;
